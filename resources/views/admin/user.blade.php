494d3d7deb29524c54aae6c37aaf06aff69dc354
Sang Pham <sangphamyn313@gmail.com>
2023-09-18 01:02:10 +0700

    update admin/user view nap tien

--- resources/views/admin/user.blade.php
+++ resources/views/admin/user.blade.php
@@ -1,6 +1,11 @@
 @extends('admin_main')
 @section('content')
-
+<style>
+  .btn.btn-danger.dropdown-toggle::after,
+  .btn.btn-success.dropdown-toggle::after {
+    display: none;
+  }
+</style>
 <div class="container">
     <div class="row">
       <div class="col-12">
@@ -19,21 +24,36 @@
                   <td>{{$user->name}}</td>
                   <td>{{number_format($user->sodu)}} đ</td>
                   <td>
-                      <div class="dropdown">
-                        <button class="btn btn-danger dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                          <i class="las la-lock"></i> Khóa
-                        </button>
-                        <ul class="dropdown-menu p-2 text-center">
-                          <form action="/admin/user/block?id={{$user->id}}" method="POST">
-                            <div class="mb-2">
-                              <label for="reason" class="text-sm font-medium mb-1 inline-block">Lý do</label>
-                              <textarea name="reason" id="reason" class="input-form w-full px-3 py-2 border border-gray-300 text-sm" cols="30" rows="2">{{ old('description') }}</textarea>
-                            </div>
-                            {{ csrf_field() }}
-                            <input type="submit" name="block" value="Khóa" class="btn btn-success">
-                          </form>
-                        </ul>
-                      </div>
+                    <div class="dropdown inline" style="display: inline">
+                      <button class="btn btn-success dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="las la-money-bill"></i> Nạp tiền
+                      </button>
+                      <ul class="dropdown-menu p-2 text-center">
+                        <form action="/admin/user/block?id={{$user->id}}" method="POST">
+                          <div class="mb-2">
+                            <label for="money" class="text-sm font-medium mb-1 inline-block">Số tiền</label>
+                            <input name="money" id="money" class="input-form w-full px-3 py-2 border border-gray-300 text-sm" type="number"/>
+                          </div>
+                          {{ csrf_field() }}
+                          <input type="submit" name="block" value="Nạp" class="btn btn-success">
+                        </form>
+                      </ul>
+                    </div>
+                    <div class="dropdown inline" style="display: inline">
+                      <button class="btn btn-danger dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="las la-lock"></i> Khóa
+                      </button>
+                      <ul class="dropdown-menu p-2 text-center">
+                        <form action="/admin/user/block?id={{$user->id}}" method="POST">
+                          <div class="mb-2">
+                            <label for="reason" class="text-sm font-medium mb-1 inline-block">Lý do</label>
+                            <textarea name="reason" id="reason" class="input-form w-full px-3 py-2 border border-gray-300 text-sm" cols="30" rows="2">{{ old('description') }}</textarea>
+                          </div>
+                          {{ csrf_field() }}
+                          <input type="submit" name="block" value="Khóa" class="btn btn-success">
+                        </form>
+                      </ul>
+                    </div>
                   </td>
                 </tr>
             @endforeach
