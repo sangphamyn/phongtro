e1c9b07954d248d4e602c9ee09fd3f3e5396ae7f
unknown <ng.vanhai2205@gmail.com>
2023-09-17 23:25:42 +0700

    list post home, orderby post

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,8 +6,11 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Models\District;
 use App\Models\Service;
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
 
 class LoginController extends Controller
 {
@@ -31,10 +34,23 @@ class LoginController extends Controller
     public function home() {
         $districts = District::get();
         $services = Service::get();
+
+        $new_posts = Post::where('trangthai','=','1')->with('huyen')->with('xa')->with('services')->with('images')->orderBy('created_at', 'desc')->paginate(8);
+        $tptn = DB::select('select count(id) as a from posts where id_dt = 1');
+        $tppy = DB::select('select count(id) as a from posts where id_dt = 8');
+        $tpsc = DB::select('select count(id) as a from posts where id_dt = 2');
+        $hpb = DB::select('select count(id) as a from posts where id_dt = 9');
+        $hdt = DB::select('select count(id) as a from posts where id_dt = 7');
         return view('home', [
             'title' => "Trang chủ",
             'districts' => $districts,
-            'services' => $services
+            'services' => $services,
+            'tptn' => $tptn[0]->a,
+            'tppy' => $tppy[0]->a,
+            'tpsc' => $tpsc[0]->a,
+            'hpb' => $hpb[0]->a,
+            'hdt' => $hdt[0]->a,
+            'new_posts'=>$new_posts
         ]);
     }
 }
